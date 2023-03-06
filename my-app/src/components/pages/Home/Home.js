b7eff03f57eff8a7c851eb6739a2bbf0546d6e89
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2023-03-06 14:43:31 +0000

    re-arranged folder structure

--- my-app/src/components/pages/Home/Home.js
+++ my-app/src/components/pages/Home/Home.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Jumbotron from "../Jumbotron";
-import HomeContainer from "../HomeContainer";
+import Jumbotron from "../jumbotron/Jumbotron";
+import HomeContainer from "../homeContainer/HomeContainer";
 import "./home.css";
 
 function Home() {
   return (
-    <div id="homepage">
+
+    
+    <div id="homepage" >
       {Jumbotron()}
+        
+      <div>
       {HomeContainer()}
+      </div>
     </div>
   );
 }
---
+++ my-app/src/components/pages/about/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function About() {
+  return (
+    <div>
+      <p>About!</p>
+
+
+    </div>
+  );
+}
+
+export default About;
