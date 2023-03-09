5883813ab3c8230e5cd71d59dabbad818cb98dbe
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2023-03-09 13:58:29 +0000

    added a pdf cv to resume page

--- my-app/src/components/pages/resume/Resume.js
+++ my-app/src/components/pages/resume/Resume.js
@@ -1,17 +1,26 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./resume.css";
+import Pdf from "./Programmer-CV-Peter-Andrew.pdf";
 
 function Resume() {
-  return (
-
-             
+  return (     
 
     <div id="resumeContainer" className="container-fluid flex justify-content-center">
-     <h1>Welcome to my resumé!</h1>
+     
+      <div id="innerResumeContainer">  
+        <h1>Welcome to my resumé!</h1>
         
-        <p>This part is inside a .container class.</p> 
+        {/* <p>This part is inside a .container class.</p>  */}
         
+      
+        <h1>CV</h1>
+
+             
+        <a href = {Pdf} target = "_blank">Click this link to open resumé Pdf</a>
+ 
+        </div>
+
       </div>
 
       
